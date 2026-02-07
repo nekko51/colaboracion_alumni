703d8f95bd04dd67e9dfe4c14674fa7b23d1a3df
Marta <martablombarte@gmail.com>
2026-02-07 10:23:35 +0100

    Coloreado de amino según clas RasMol, (he tenido cuidado Iñigo)

---
+++ code/coloring_elbueno.py
@@ -0,0 +1,43 @@
+#Definición de la función que a cada aminoacid le asigna su color siguiente la clasificación RasMol
+import numpy as np
+
+def coloring_function (orden):
+
+    #Usamos el diccionario de colores otorgado por RasMol
+
+    rasmol_rgb = {
+        'A': [200, 200, 200],  # Dark Grey
+        'C': [230, 230, 0],    # Yellow
+        'D': [230, 10, 10],    # Bright Red 
+        'E': [230, 10, 10],    # Bright Red
+        'F': [50, 50, 170],    # Mid Blue
+        'G': [235, 235, 235],  # Light Grey
+        'H': [130, 130, 210],  # Pale Blue
+        'I': [15, 130, 15],    # Green
+        'K': [20, 90, 255],    # Blue
+        'L': [15, 130, 15],    # Green
+        'M': [230, 230, 0],    # Yellow
+        'N': [0, 220, 220],    # Cyan
+        'P': [220, 150, 130],  # Flesh
+        'Q': [0, 220, 220],    # Cyan
+        'R': [20, 90, 255],    # Blue
+        'S': [250, 150, 0],    # Orange
+        'T': [250, 150, 0],    # Orange
+        'V': [15, 130, 15],    # Green
+        'W': [180, 90, 180],   # Purple
+        'Y': [50, 50, 170],    # Mid Blue
+        '-': [190, 160, 110]   # Tan (Usando la categoría 'Others' para el hueco)
+    }
+
+    colores = []  #Creamos una lista vacía donde vamos a incluir el color de cada aminoácido
+
+    for aa in (orden):
+        rgb = rasmol_rgb.get (aa)  #Cogemos un aminoacid del diccionario
+
+        if rgb:
+            rgb_normalizado = [valor / 255.0 for valor in rgb]  #Debemos normalizarlo pues Matplotlib
+            colores.append(rgb_normalizado)                     #espera valores entre 0 y 1.
+        else:
+            colores.append([1.0, 1.0, 1.0])  #Si hay algún defecto se le asigna el color negro
+
+    return np.array(colores)
