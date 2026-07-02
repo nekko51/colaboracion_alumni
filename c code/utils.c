73a2dcabb1e591f96f1b3465b52918a6d471b716
Ilakyyyy <924640@unizar.es>
2026-07-02 13:43:57 +0200

    med_var function

--- c code/utils.c
+++ c code/utils.c
@@ -1,2 +1,21 @@
 #include "head.h"
 
+void med_var(double* data, int n, double* mean, double* variance) {
+    int i;
+    double sum, res;
+    *mean=0;
+    *variance=0;
+
+    sum = 0.0;
+    for (i=0; i<n; i++){
+        sum += data[i];
+    }
+    *mean = sum/n;
+
+    res = 0.0;
+    for(i=0; i<n; i++){
+        res += (data[i] - *mean) * (data[i] - *mean);//pow makes e^ln
+    }
+
+    *variance = res/(n-1);
+}
